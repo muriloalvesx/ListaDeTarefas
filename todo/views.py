f265629cb27af012b267f9d3162123bade266e2a
Murilo Alves dos Santos <153781890+muriloalvesx@users.noreply.github.com>
2024-06-04 15:58:27 -0300

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -45,7 +45,8 @@ def loginn(request):
             return redirect('/loginn')
                
     return render(request, 'loginn.html')
-        
+
+@login_required(login_url='/loginn')
 def todopage(request):
     if request.method == 'POST':
         title=request.POST.get('title')
